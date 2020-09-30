901e80481f7c655bc8351ef91b36b3b8579657da
Fuyj <2312696405@qq.com>
2020-09-30 18:35:08 +0800

    user finished

--- app.js
+++ app.js
@@ -50,11 +50,13 @@ const serverHandle = ((req, res) => {
             return
         }
         //处理user路由
-        const userData = handleUserRouter(req, res)
-        if (userData) {
-            res.end(
-                JSON.stringify(userData)
-            )
+        const userResult = handleUserRouter(req, res)
+        if (userResult) {
+            userResult.then(userData => {
+                res.end(
+                    JSON.stringify(userData)
+                )
+            })
             return
         }
         //未命中路由返回404
