57e6f2c17da1bfba4d795d6afbd619321104bd76
Fuyj <2312696405@qq.com>
2020-10-07 08:31:07 +0800

    login finshed

--- src/router/blog.js
+++ src/router/blog.js
@@ -1,14 +1,33 @@
 const { getList, getDetail, newBlog, updateBlog, delBlog } = require('../controller/blog')
 const { SuccessModel, ErrorModel } = require('../model/resModel')
+
+// 统一的验证登录函数
+const  loginCheck =(req)=>{
+    if(!req.session.username){
+        return Promise.resolve(
+            new ErrorModel('尚未登录')
+        )
+    }
+}
 const handleBlogRouter = (req, res) => {
     const method = req.method
     const id = req.query.id
     //获取博客列表
     if (method === "GET" && req.path === "/api/blog/list") {
-        const author = req.query.author || ``
+        let author = req.query.author || ``
         const keyword = req.query.keyword || ``
         // const listData = getList(author, keyword)
         // return new SuccessModel(listData)
+        if(req.query.isadmin){
+            //管理员界面
+            const loginCheckResult = loginCheck(req)
+            if(loginCheckResult){
+                //未登录
+                return loginCheckResult
+            }
+            //强行查询自己的博客
+            author = req.session.username
+        }
         const result = getList(author, keyword)
         return result.then(listData => {
             return new SuccessModel(listData)
@@ -24,7 +43,11 @@ const handleBlogRouter = (req, res) => {
 
     //新建一篇博客
     if (method === "POST" && req.path === "/api/blog/new") {
-        req.body.author = 'zhangsan'//假数据 开发登录时再改成真实数据
+        const loginCheckResult = loginCheck(req)
+        if(loginCheckResult){
+            return loginCheckResult
+        }
+        req.body.author = req.session.username
         const result = newBlog(req.body)
         return result.then(data => {
             return new SuccessModel(data)
@@ -33,6 +56,10 @@ const handleBlogRouter = (req, res) => {
     }
     //更新一篇博客
     if (method === "POST" && req.path === "/api/blog/update") {
+        const loginCheckResult = loginCheck(req)
+        if(loginCheckResult){
+            return loginCheckResult
+        }
         const result = updateBlog(id, req.body)
         return result.then(val => {
             if (val) {
@@ -46,7 +73,11 @@ const handleBlogRouter = (req, res) => {
     }
     //删除一篇博客
     if (method === "POST" && req.path === "/api/blog/del") {
-        req.body.author = 'zhangsan'//假数据 开发登录时再改成真实数据
+        const loginCheckResult = loginCheck(req)
+        if(loginCheckResult){
+            return loginCheckResult
+        }
+        req.body.author = req.session.username
         const result = delBlog(id,author);
         return result.then(val => {
             if (val) {
--- src/router/user.js
+++ src/router/user.js
@@ -4,9 +4,9 @@ const { SuccessModel, ErrorModel } = require('../model/resModel')
 const handleUserRouter = (req, res) => {
     const method = req.method
     //获取博客列表
-    if (method === "GET" && req.path === "/api/user/login") {
-        // const { username, password } = req.body
-        const { username, password } = req.query
+    if (method === "POST" && req.path === "/api/user/login") {
+        const { username, password } = req.body
+        // const { username, password } = req.query
         const result = login(username, password)
         return result.then(data => {
             if (data.username) {
@@ -20,15 +20,15 @@ const handleUserRouter = (req, res) => {
         )
     }
     //登录验证的测试
-    if (method === 'GET' && req.path === "/api/user/login-test") {
-        if ( req.session.username) {
-            return Promise.resolve(new SuccessModel(
-                {
-                    session:  req.session
-                }
-            ))// Promise.resolve 可以直接返回一个promise
-        }
-        return Promise.resolve(new ErrorModel('尚未登录'))
-    }
+    // if (method === 'GET' && req.path === "/api/user/login-test") {
+    //     if ( req.session.username) {
+    //         return Promise.resolve(new SuccessModel(
+    //             {
+    //                 session:  req.session
+    //             }
+    //         ))// Promise.resolve 可以直接返回一个promise
+    //     }
+    //     return Promise.resolve(new ErrorModel('尚未登录'))
+    // }
 }
 module.exports = handleUserRouter
