4196eb776c1c9d0db82c360a0aa6baf1678cf5d2
Fuyj <2312696405@qq.com>
2020-10-12 08:11:15 +0800

    routes finished

--- blog-express/routes/blog.js
+++ blog-express/routes/blog.js
@@ -1,5 +1,6 @@
 const { getList, getDetail, newBlog, updateBlog, delBlog } = require('../controller/blog')
 const { SuccessModel, ErrorModel } = require('../model/resModel')
+const loginCheck = require('../middleware/loginCheck')
 const express = require('express')
 
 const router = express.Router()
@@ -7,21 +8,58 @@ const router = express.Router()
 router.get('/list', (req, res, next) => {
     let author = req.query.author || ``
     const keyword = req.query.keyword || ``
-  
-    // if (req.query.isadmin) {
-    //     //管理员界面
-    //     const loginCheckResult = loginCheck(req)
-    //     if (loginCheckResult) {
-    //         //未登录
-    //         return loginCheckResult
-    //     }
-    //     //强行查询自己的博客
-    //     author = req.session.username
-    // }
+
+    if (req.query.isadmin) {
+        //管理员界面
+        if (req.session.username === null) {
+            //未登录
+            res.json(new ErrorModel('未登录'))
+            return
+        }
+        //强行查询自己的博客
+        author = req.session.username
+    }
     const result = getList(author, keyword)
     return result.then(listData => {
-       res.json( new SuccessModel(listData))
+        res.json(new SuccessModel(listData))
     })
 })
 
+router.get('/detail', (req, res, next) => {
+    const result = getDetail(req.query.id)
+    return result.then(data => {
+        res.json(new SuccessModel(data))
+    })
+})
+
+router.post('/new', loginCheck, (req, res, next) => {
+    req.body.author = req.session.username
+    const result = newBlog(req.body)
+    return result.then(data => {
+        res.json(new SuccessModel(data))
+    })
+})
+router.post('/update', loginCheck, (req, res, next) => {
+    const result = updateBlog(req.body.id, req.body)
+    return result.then(val => {
+        if (val) {
+            res.json(new SuccessModel())
+            return
+        } else {
+            res.json(new ErrorModel('更新博客失败'))
+        }
+    })
+})
+
+router.post('/del', loginCheck, (req, res, next) => {
+    req.body.author = req.session.username
+    const result = delBlog(req.query.id, author);
+    return result.then(val => {
+        if (val) {
+            res.json(new SuccessModel()) 
+        } else {
+            res.json(new ErrorModel('删除博客失败')) 
+        }
+    })
+})
 module.exports = router
