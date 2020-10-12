5376f125ae7a5233bc80f3826cc96fb7cc44773d
Fuyj <2312696405@qq.com>
2020-10-12 12:09:04 +0800

    init koa2 project and session redis finished

---
+++ blog-koa2/app.js
@@ -0,0 +1,63 @@
+const Koa = require('koa')
+const app = new Koa()
+const views = require('koa-views')
+const json = require('koa-json')
+const onerror = require('koa-onerror')
+const bodyparser = require('koa-bodyparser')
+const logger = require('koa-logger')
+const session = require('koa-generic-session')
+const redisStroe = require('koa-redis')
+
+const index = require('./routes/index')
+const users = require('./routes/users')
+const blog = require('./routes/blog')
+const user = require('./routes/user')
+
+// error handler
+onerror(app)
+
+// middlewares
+app.use(bodyparser({
+  enableTypes: ['json', 'form', 'text']
+}))
+app.use(json())
+app.use(logger())//日志
+app.use(require('koa-static')(__dirname + '/public'))
+
+app.use(views(__dirname + '/views', {
+  extension: 'pug'
+}))
+
+// logger
+app.use(async (ctx, next) => {
+  const start = new Date()
+  await next()
+  const ms = new Date() - start
+  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
+})
+
+app.keys = ['Wj1010_2000!']
+app.use(session({
+  //配置cookie
+  cookie: {
+    path: '/',
+    httpOnly: true,
+    maxAge: 24 * 60 * 60 * 1000
+  },
+  //配置redis
+  store: redisStroe({
+    all: '127.0.0.1:6379'//写死本地的redis
+  })
+}))
+
+// routes
+app.use(index.routes(), index.allowedMethods())
+app.use(users.routes(), users.allowedMethods())
+app.use(blog.routes(), blog.allowedMethods())
+app.use(user.routes(), user.allowedMethods())
+// error-handling
+app.on('error', (err, ctx) => {
+  console.error('server error', err, ctx)
+});
+
+module.exports = app
